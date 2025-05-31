0fb76d95805796feeeb28f92170288ebcdb81d82
kadelcode <kadelcode@gmail.com>
2025-05-31 20:30:17 +0100

    Add forgot password link

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -134,7 +134,7 @@ export default function LoginPage() {
                             </div>
 
                             <div className="text-sm">
-                                <a href="#" className="font-medium text-indigo-600 hover:text-indigo-500">
+                                <a href="/forgot-password" className="font-medium text-indigo-600 hover:text-indigo-500">
                                     Forgot your password?
                                 </a>
                             </div>
